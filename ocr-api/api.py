7222a074df1c99e8b4389c4589519f3fd011aa6f
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2025-12-30 23:29:35 +0100

    style: added swagger ui to get /

--- ocr-api/api.py
+++ ocr-api/api.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from PIL import Image
 import pytesseract
 import io
+from fastapi.openapi.docs import get_swagger_ui_html
 
 app = FastAPI(title="OCR API")
 
@@ -18,3 +19,7 @@ async def ocr_endpoint(file: UploadFile = File(...)):
     # OCR
     text = pytesseract.image_to_string(img)
     return {"filename": file.filename, "text": text}
+
+@app.get("/", include_in_schema=False)
+async def custom_swagger_ui():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="OCR API Docs")
